bde18a1f3977b490b30d404d2eb1451c9b9c4611
Elizaveta <elis1386@gmail.com>
2021-07-20 14:14:59 +0300

    added lesson1

---
+++ lesson1/scripts/productsCards.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const products = [
+    new ProductDTO(0, 'item-1.png', 'Mango People T-shirt', 52.32,),
+    new ProductDTO(1, 'item-2.png', 'Mango People Blazer', 23.04,),
+    new ProductDTO(2, 'item-3.png', 'Mango People Jacket', 52.90,),
+    new ProductDTO(3, 'item-4.png', 'Mango People Dress', 56.54,),
+]
+
+
+function getProductMarkup(product) {
+    const pathToImages = 'img';
+    return `
+        <div class="item">
+            <a class="product"
+                href="single page.html">
+                <img alt="product photo"
+                        class="product-img"
+                        src="${pathToImages}/${product.image}">
+                <div class="product-text-box">
+                    <p class="product-text">${product.name}</p>
+                    <p class="product-price">${product.price}</p>
+                </div>
+            </a>
+            <div class="box-add">
+                <a class="add" dataproductId = "${product.id}" href="#"><img class="add-img" src="${pathToImages}/cart_add.svg" alt="cart-img">Add to
+                    cart</a>
+            </div>
+        </div>
+    `;
+}
+
+function addProducts(products) {
+    let productsMarkup = '';
+    for (let product of products) {
+        productsMarkup += getProductMarkup(product);
+    }
+    const boxProducts = document.querySelector('.box-product');
+    boxProducts.innerHTML = productsMarkup;
+}
+
+addProducts(products);
