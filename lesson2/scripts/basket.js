0eb521a4af7a6ff46811a0b4ad820b24b347855f
Elizaveta <elis1386@gmail.com>
2021-07-27 13:32:04 +0300

    added lesson2

---
+++ lesson2/scripts/basket.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const basketProducts = [
+    new Basket(0, 'item-1.png', 'Mango People T-shirt', 52.32),
+    new Basket(1, 'item-2.png', 'Mango People Blazer', 23.04),
+    new Basket(2, 'item-3.png', 'Mango People Jacket', 52.90),
+    new Basket(3, 'item-4.png', 'Mango People Dress', 56.54),
+]
